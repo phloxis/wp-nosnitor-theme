197d7530b6c85d7614fd078be4301eee0b7ddd5f
Jeff Block <jsblock78@outlook.com>
2019-10-12 02:29:20 -0600

    Parameterized entrypoint

--- docker/wp-cli/docker-entrypoint-override.sh
+++ docker/wp-cli/docker-entrypoint-override.sh
@@ -4,7 +4,7 @@
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' wordpress:80)" != "302" ]]; do echo ''Waiting for wordpress to come up...''; sleep 1; done'
 
 # perform wordpress install
-wp core install --url=http://localhost:8080 --title="My Blog" --admin_user=admin --admin_password=pass --admin_email=wp-admin@nosnitor.net --skip-email --allow-root
+wp core install --url=$WPCORE_URL --title="$WPCORE_TITLE" --admin_user="$WPCORE_ADMINUSER" --admin_password="$WPCORE_ADMINPASS" --admin_email="$WPCORE_ADMINEMAIL" --skip-email --allow-root
 
 # enable nosnitor theme
 wp theme activate nosnitor --allow-root
