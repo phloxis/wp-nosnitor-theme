990a1dc8cc855d4ae082aaada315d7b1d79004a4
Jeff Block <jsb@phloxis.com>
2019-10-12 13:21:28 -0600

    Activate theme after wp configuration

--- docker/wp-cli/docker-entrypoint-override.sh
+++ docker/wp-cli/docker-entrypoint-override.sh
@@ -6,11 +6,11 @@ bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' wordpress:80)" != 
 # perform wordpress install
 wp core install --url=$WPCORE_URL --title="$WPCORE_TITLE" --admin_user="$WPCORE_ADMINUSER" --admin_password="$WPCORE_ADMINPASS" --admin_email="$WPCORE_ADMINEMAIL" --skip-email --allow-root
 
-# enable nosnitor theme
-wp theme activate nosnitor --allow-root
-
 # configure permalinks
 wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/' --allow-root
+
+# enable nosnitor theme
+wp theme activate nosnitor --allow-root
 
 # load default entrypoint
 #docker-entrypoint.sh wp shell
